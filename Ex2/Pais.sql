f65c9762cafdc6536319cf1961647eebe125fb4a
Joao Varella <36937320+sxreven@users.noreply.github.com>
2018-03-15 17:40:58 -0300

    Update Pais.sql

--- Ex2/Pais.sql
+++ Ex2/Pais.sql
@@ -1,9 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS `Pais` DEFAULT CHARACTER SET utf8 ;
 USE `Pais` ;
 
--- -----------------------------------------------------
--- Table `vendas`.`Cliente`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Pais (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
