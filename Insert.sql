c38bf26f6be0a3ad39860c36e2e2e7b23a96677d
Joao Varella <36937320+sxreven@users.noreply.github.com>
2018-03-15 17:30:38 -0300

    Delete Insert.sql

--- Insert.sql
+++
@@ -1,6 +0,0 @@
-USE PAIS;
-
-insert into `Pais` (`id`, `nome`, `populacao`, `area`) Values (1, 'Brasil', '207700000', '8516000' );
-insert into `Pais` (`id`, `nome`, `populacao`, `area`) Values (2, 'Argentina', '43850000', '2780000');
-insert into `Pais` (`id`, `nome`, `populacao`, `area`) Values (3, 'Chile', '17910000', '756102');
-insert into `Pais` (`id`, `nome`, `populacao`, `area`) Values (4, 'Cuba', '11480000', '109884');
